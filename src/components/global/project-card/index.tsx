e3265aac11c45a9db452c6fd615abb325a8ef64f
huzwarsi <huzaifawarsi2006@gmail.com>
2026-06-03 22:32:30 +0500

    updated

--- src/components/global/project-card/index.tsx
+++ src/components/global/project-card/index.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { itemVariants } from '@/lib/constants';
+import { useSlideStore } from '@/store/useSlideStore';
+import { JsonValue } from '@prisma/client/runtime/client';
+import motion from 'framer-motion'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 type Props = {
@@ -12,11 +17,35 @@ type Props = {
 
 }
 
-const ProjectCard = (props: Props) => {
-    return (
-        <div> </div>
+const ProjectCard = ({
+    projectId, title, createdAt, slideData, src, isDelete
+}: Props) => {
 
+    const { setSlides } = useSlideStore()
+    const router = useRouter()
+    const handleNavigation = () => {
+        setSlides(JSON.parse(JSON.stringify(slideData)))
+        router.push(`/presentaion/${projectId}`)
+
+    }
+
+    return (
+
+        <motion.div
+            className={`group w-full flex flex-col gap-y-3 rounded-xl p-3 transition-colors ${!isDelete && 'hover:bg-muted/50'
+                }`}
+            variants={itemVariants}
+        >
+
+            <div
+                className="relative aspect-[16/10] overflow-hidden rounded-lg cursor-pointer"
+                onClick={handleNavigation}
+            >
+            </div>
+
+        </motion.div>
     )
+
 }
 
 export default ProjectCard
