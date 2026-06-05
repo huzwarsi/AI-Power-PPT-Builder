1a2e5947e783f289bda8571e35dfd8be2ce92d4f
huzwarsi <huzaifawarsi2006@gmail.com>
2026-06-05 19:23:52 +0500

    feat: implement ProjectCard component with project deletion and recovery functionality

--- src/actions/projects.ts
+++ src/actions/projects.ts
@@ -70,3 +70,67 @@ export const getRecentProjects = async () => {
 
     }
 }
+
+export const recoverProject = async (projectId: string) => {
+
+    try {
+        const checkUser = await onAuthenticateUser()
+
+        if (checkUser.status !== 200 || !checkUser.user) {
+            return { status: 403, error: 'User Not Authenticated' }
+        }
+
+        const updatedProject = await client.project.update({
+            where: {
+                id: projectId,
+            },
+            data: {
+                isDeleted: false
+            },
+
+        })
+        if (!updatedProject) {
+            return { status: 500, error: 'Failed to recover Project' }
+
+        }
+
+        return { status: 200, data: updatedProject }
+
+    } catch (error) {
+        console.log('🔴 ERROR', error)
+        return { status: 500, error: 'Internal Server error' }
+    }
+}
+
+
+
+
+export const DeleteProject = async (projectId: string) => {
+    try {
+        const checkUser = await onAuthenticateUser()
+
+        if (checkUser.status !== 200 || !checkUser.user) {
+            return { status: 403, error: 'User Not Authenticated' }
+        }
+
+        const updatedProject = await client.project.update({
+            where: {
+                id: projectId,
+            },
+            data: {
+                isDeleted: true
+            },
+
+        })
+        if (!updatedProject) {
+            return { status: 500, error: 'Failed to delete Project' }
+
+        }
+
+        return { status: 200, data: updatedProject }
+
+    } catch (error) {
+        console.log('🔴 ERROR', error)
+        return { status: 500, error: 'Internal Server error' }
+    }
+}
--- src/components/global/project-card/index.tsx
+++ src/components/global/project-card/index.tsx
@@ -8,12 +8,14 @@ import React, { useState } from 'react'
 import ThumbnailPreview from './Thumbnail-preview';
 import { timeAgo } from '@/lib/utils';
 import AlertDialogBox from '../AlertDialogBox';
+import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
+import { DeleteProject, recoverProject } from '@/actions/projects';
 
 type Props = {
     projectId: string
     title: string
     createdAt: string
-    src: string
     isDelete?: boolean
     slideData: JsonValue
     themeName: string
@@ -22,7 +24,7 @@ type Props = {
 }
 
 const ProjectCard = ({
-    projectId, title, createdAt, slideData, src, isDelete, themeName
+    projectId, title, createdAt, slideData, isDelete, themeName
 }: Props) => {
 
     const [loading, setLoading] = useState(false)
@@ -37,6 +39,79 @@ const ProjectCard = ({
     }
 
     const theme = themes.find((t) => t.name === themeName) || themes[0]
+
+    const handleRecover = async () => {
+        setLoading(true)
+        if (!projectId) {
+            setLoading(false)
+            toast('Error', {
+                description: 'Project not found.'
+            })
+            return
+
+        }
+        try {
+            const res = await recoverProject(projectId)
+            if (res.status !== 200) {
+                toast.error('Oppse', {
+                    description: 'Something went wrong'
+                })
+            }
+            setOpen(false)
+            router.refresh()
+            toast.success('Success', {
+                description: 'Project recovered successfully'
+            })
+        } catch (error) {
+            console.log(error);
+
+            toast.error('Oppse', {
+                description: 'Something went wrong please contact support'
+            })
+        }
+    }
+
+
+
+
+    const handleDelete = async () => {
+        setLoading(true);
+
+        if (!projectId) {
+            setLoading(false);
+            toast.error('Error', {
+                description: 'Project not found.'
+            });
+            return;
+        }
+
+        try {
+            const res = await DeleteProject(projectId);
+
+            if (res.status !== 200) {
+                toast.error('Oops', {
+                    description: 'Failed to delete the project'
+                });
+                return;
+            }
+
+            setOpen(false);
+            router.refresh();
+
+            toast.success('Success', {
+                description: 'Project Deleted successfully'
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            toast.error('Oops', {
+                description: 'Something went wrong please contact support'
+            });
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
 
@@ -61,27 +136,57 @@ const ProjectCard = ({
                 <div className='space-y-1'>
                     <h3 className='font-semibold text-base text-primary line-clamp-1'>
 
-                        {title}
+                        {title} This is the title that I want to see
                     </h3>
                     <div className='flex w-full justify-between items-center gap-2'>
                         <p
                             className='text-sm text-muted-foreground' suppressHydrationWarning  >
                             {timeAgo(createdAt)}
                         </p>
-                        {/* {isDelete ? ( */}
-                        <AlertDialogBox
-                            className='bg-green-500 test-white dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700'
-                            description='This will recover yor project and restore your data.'
-                            loading={loading}
-                            open={open}
-                        >
+                        {isDelete ? (
+                            <AlertDialogBox
+                                className='bg-green-500 text-white dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700'
+                                description='This will recover yor project and restore your data.'
+                                loading={loading}
+                                open={open}
+                                onClick={handleRecover}
+                                handleOpen={() => setOpen(!open)}
+                            >
+
+                                <Button
+                                    size="sm"
+                                    variant="ghost"
+                                    className="bg-background-80 dark:hover:bg-background-90"
+                                    disabled={loading}
+                                >
+                                    Recover
+                                </Button>
 
 
-                        </AlertDialogBox>
+                            </AlertDialogBox>
 
-                        {/* ): (
-                            ''
-                        )} */}
+                        ) : (
+                            <AlertDialogBox
+                                className='bg-red-500 text-white dark:bg-red-600 hover:bg-red-600 dark:hover:bg-red-700'
+                                description='This will recover yor project and restore your data.'
+                                onClick={handleDelete}
+                                loading={loading}
+                                open={open}
+                                handleOpen={() => setOpen(!open)}
+                            >
+
+                                <Button
+                                    size="sm"
+                                    variant="ghost"
+                                    className="bg-background-80 dark:hover:bg-background-90"
+                                    disabled={loading}
+                                >
+                                    Delete
+                                </Button>
+
+
+                            </AlertDialogBox>
+                        )}
                     </div>
                 </div>
 
